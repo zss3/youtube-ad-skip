c3ee9612182688cfb8756fbddb927022ee81f8c8
Zack Sempsrott <zsempsrott@yahoo.com>
2024-11-10 17:39:57 -0500

    Update for changes on youtube.

--- skip.js
+++ skip.js
@@ -1,17 +1,24 @@
 document.addEventListener("yt-navigate-finish", function(event) {
  // Main callback function to execute when mutations are observed
     const callback = function(mutationsList, observer) {
-        let skipButton = document.getElementsByClassName("ytp-ad-skip-button-container")[0];
-        let previewSlot = document.getElementsByClassName("ytp-ad-preview-slot")[0];
-        let skipSlot = document.getElementsByClassName("ytp-ad-skip-button-slot")[0];
+        let skipButton = document.getElementsByClassName("ytp-skip-ad-button")[0];
+        let previewSlot = document.getElementsByClassName("ytp-preview-ad")[0];
+        let skipSlot = document.getElementsByClassName("ytp-skip-ad")[0];
         let overlayCloseBtn = document.getElementsByClassName("ytp-ad-overlay-close-button")[0];
 
         // if there's a video ad, skip it. 
-        if(skipSlot){
-        previewSlot.style.display = "none"
-        skipSlot.style.display = "inherit"
-        skipButton.click();
-        console.log("Ad skipped.")
+        if(skipSlot && skipButton){
+            // An additional attempt to skip an ad using a different approach. 
+            // YT is changing their design which can impact the ability to automate the clicking of the skip button.
+            const video = document.getElementsByClassName("video-stream html5-main-video")[0];
+            video.currentTime = video.duration;
+            console.log("Fast-Forwarding to end of ad.");
+
+            previewSlot.style.display = "none"
+            skipButton.style.display = "block"
+            skipSlot.click();
+            skipButton.click();
+            console.log("Ad skipped.")
         }
 
         // if there's an overlay ad, close it.
